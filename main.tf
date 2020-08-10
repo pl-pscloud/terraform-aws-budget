e901c2ee7d00dec9720daf6e434ef94a5484a558
pswitala <Da39a3ee5G>
2020-08-10 14:52:20 +0200

    First commit module terraform-aws-budget

---
+++ main.tf
@@ -0,0 +1,18 @@
+resource "aws_budgets_budget" "pscloud-budget" {
+  name              = "${var.pscloud_company}_budget_${var.pscloud_period}_${var.pscloud_env}"
+  budget_type       = var.pscloud_budget_type
+  limit_amount      = var.pscloud_limit_amount
+  limit_unit        = var.pscloud_limit_unit
+  time_period_start = var.pscloud_time_period_start
+  time_period_end   = var.pscloud_time_period_end
+  time_unit         = var.pscloud_period
+
+  
+  notification {
+    comparison_operator        = var.pscloud_comparison_operator
+    threshold                  = var.pscloud_threshold
+    threshold_type             = var.pscloud_threshold_type
+    notification_type          = var.pscloud_notification_type
+    subscriber_email_addresses = var.pscloud_subscriber_email_addresses
+  }
+}
---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "pscloud_env" {}
+variable "pscloud_company" {}
+
+variable "pscloud_period" { default = "monhtly" }
+variable "pscloud_limit_unit" { default = "USD" }
+variable "pscloud_budget_type" { default = "COST"}
+variable "pscloud_limit_amount"  {}
+variable "pscloud_time_period_end" {} 
+variable "pscloud_time_period_start" {}
+
+variable "pscloud_comparison_operator"  { default = "GREATER_THAN" }
+variable "pscloud_threshold" { default = 100 }
+variable "pscloud_threshold_type" {default = "PERCENTAGE" }
+variable "pscloud_notification_type" { default = "FORECASTED" }
+variable "pscloud_subscriber_email_addresses" { }
